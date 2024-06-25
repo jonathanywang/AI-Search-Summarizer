1479ee7fb0cbf764e044ddc011f22403d84737f8
jonathanywang <jonathanywang@users.noreply.github.com>
2024-06-25 10:33:47 -0400

    summarizer api capabilities

---
+++ main.py
@@ -0,0 +1,51 @@
+import openai
+from exa_py import Exa
+from datetime import datetime, timedelta
+import textwrap
+
+
+EXA_API_KEY = 000 # Exa API Key here
+OPENAI_API_KEY = 000 # OpenAI API Key here
+
+openai.api_key = OPENAI_API_KEY
+exa = Exa(EXA_API_KEY)
+
+SYSTEM_MESSAGE = "You are a helpful assistant that generates search queries based on user questions. Only generate one search query."
+USER_QUESTION = "What's the recent news in physics this week?"
+
+completion = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": SYSTEM_MESSAGE},
+        {"role": "user", "content": USER_QUESTION},
+    ],
+)
+
+search_query = completion.choices[0].message.content
+
+one_week_ago = (datetime.now() - timedelta(days=7))
+date_cutoff = one_week_ago.strftime("%Y-%m-%d")
+
+search_response = exa.search_and_contents(
+    search_query, use_autoprompt=True, start_published_date=date_cutoff
+)
+
+urls = [result.url for result in search_response.results]
+
+results = search_response.results
+result_item = results[0]
+
+
+completion = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": SYSTEM_MESSAGE},
+        {"role": "user", "content": result_item.text},
+    ],
+)
+
+summary = completion.choices[0].message.content
+
+print(f"Summary for {urls[0]}:")
+print(result_item.title)
+print(textwrap.fill(summary, 80))
